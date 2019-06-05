cfbc586b657c519ffbb6f8f363150c0e7952d9c5
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-05 12:03:39 +0200

    Registered CustomUser in admin site and define the display of it

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdminDisplay(UserAdmin):
+    '''Definning how the display will be look in admin page'''
+    add_form = CustomUserCreationForm # Using our forms from forms.py
+    model = CustomUser
+    list_display = ['email', 'username',] # Atribiutes from CustomUser model which will be displayed in admin page as list
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdminDisplay)
+
