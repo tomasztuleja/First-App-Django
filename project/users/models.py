f1732e84c829a662d37aa8a9571cdc51d6e5bee0
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-05 12:07:29 +0200

    Fixed bug with syntax

--- project/users/models.py
+++ project/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-'''Our user model that inherits from AbstractUser,
- which give us some already coded functions and other tools'''
+    '''Our user model that inherits from AbstractUser,
+    which give us some already coded functions and 
+    other tools'''
     pass
