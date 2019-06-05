fa1dbf3a63e70c20b2ef4d430fbae7f79efe2d4a
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-05 11:23:43 +0200

    Added first CustomUser model, that will be develope in the future by adding required fields

---
+++ project/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+'''Our user model that inherits from AbstractUser,
+ which give us some already coded functions and other tools'''
+    pass
