7ffe271c8f30025ca1bf9fd48ea0e445e6e364c5
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-05 12:08:57 +0200

    Fixed bug with bad import

--- project/users/admin.py
+++ project/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import CustomUserCreationForm
 from .models import CustomUser
 
 class CustomUserAdminDisplay(UserAdmin):
