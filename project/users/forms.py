df5ea25414ed901aa5f024985486d3634dea4498
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-07 14:41:53 +0200

    Fixed CustomUserCreationForm

--- project/users/forms.py
+++ project/users/forms.py
@@ -3,8 +3,19 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    '''Our register form'''
+    email = forms.EmailField(required=True) # Creating field that will be avalivale in register page
+    first_name = forms.CharField(max_length=50) # Same as above
+    last_name = forms.CharField(max_length=50) # Same as above
 
-    class Meta(UserCreationForm):
-        model = CustomUser
-        fields = ('username', 'email')
+    class Meta:
+        '''Defining our form'''
+        model = CustomUser # Definig to which model our form should be related
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2") # Fields avaliable in our form on register page
 
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
