7702ffccccbb9cbc620c357c165aa222aa7c6877
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-07 20:40:30 +0200

    Added LogInPageView, LogOutPageView, RegisterPageView and LogoutRequiredMixIn

--- project/users/views.py
+++ project/users/views.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from .models import CustomUser
 from django import forms
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -13,10 +15,33 @@ class HomePageView(TemplateView):
     template_name = 'project/home.html'
 
 
-class RegisterPageView(CreateView):
+class LogoutRequiredMixin():
+    '''Class created to redirect logged in users from register and login pages'''
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('home'))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class RegisterPageView(LogoutRequiredMixin, CreateView):
     '''Our register page, containing register form from form.py file.'''
     template_name = 'users/register.html'
-    form_class = CustomUserCreationForm
+    form_class = CustomUserCreationForm # Our form from forms.py
 
+    # Redirecting after login
     def get_success_url(self):
         return reverse('home')
+
+
+class LogInPageView(LogoutRequiredMixin ,auth_views.LoginView):
+    '''Our log in page, using form that inherits from Django LoginView.'''
+    template_name = "users/login.html"
+        
+    # Redirecting after login
+    def get_success_url(self):
+        return reverse('home')
+
+
+class LogOutPageView(auth_views.LogoutView):
+    '''Our log out page, inherited from Django LogoutView, redirected page is defined in settings.py in LOGOUT_REDIRECT_URL variable'''
+    pass
