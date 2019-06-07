45633d3575eb1cdf4635776035bbc2131801489d
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-07 20:38:50 +0200

    Added two paths for login and logout

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView, RegisterPageView
+from .views import HomePageView, RegisterPageView, LogInPageView, LogOutPageView
 
 urlpatterns=[
     path('', HomePageView.as_view(), name="home"),
     path("register", RegisterPageView.as_view(), name="register"),
+    path('login', LogInPageView.as_view(), name="login"),
+    path('logout', LogOutPageView.as_view(), name="logout"),
 ]
 
