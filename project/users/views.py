a7a1b29287397a02f66748d538e175e674278011
Lukasz Kania <lukasz.kania1996@gmail.com>
2019-06-07 14:43:26 +0200

    Fixed  RegisterPageView

--- project/users/views.py
+++ project/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse
+from .models import CustomUser
+from django import forms
 
 # Create your views here.
+class RegisterPageView(CreateView):
+    '''Our register page, containing register form from form.py file.'''
+    template_name = 'users/register.html'
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse('home')
