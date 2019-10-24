78deb0b7633330e5770e1d6da40003efbc41c364
tomasztuleja <tuleja.tomasz@gmail.com>
2019-10-24 20:37:41 +0200

    Reorganize folders and files directory

--- project/project/users/forms.py
+++ project/project/users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
 from .models import CustomUser
 from project.choices import SEX_CHOICES
 
